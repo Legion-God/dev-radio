380c0381edf75d06d81ad2e2e5ce5a7d95ab9114
Shubhendra Rajkarne <srajkarne9@gmail.com>
2021-04-15 02:46:27 +0530

    ver 1.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r', encoding="utf-8") as file:
 
 setup(
     name='dev-radio',
-    version='1.0.4',
+    version='1.0.5',
     packages=find_namespace_packages(),
     include_package_data=True,
     package_data={"": "radio_stations.json"},
